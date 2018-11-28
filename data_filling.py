dec47041541fb49770bdb57ca55e2311921e9b08
Nikko <NikkoTonoha@gmail.com>
2018-11-28 20:40:09 +0200

    add comments and support JSON

--- data_filling.py
+++ data_filling.py
@@ -1,17 +1,35 @@
 import collections
 import math
 from functools import reduce
+import json
 
 
-def filling_aver(array):
+def filling_aver(array, format_json=True):
+    """Fills the missing values ​​by a mean value
+
+    Keyword arguments:
+    array - input data array
+    format_json - determines whether to return to JSON format or list
+
+    """
+
+    if type(array) is str:
+        array = json.loads(array)
+
     full = list(filter(lambda x: not math.isnan(x), array))
 
     aver = sum(full) / len(full)
 
-    return filling(array, aver)
+    result=filling(array, aver)
+    return json.dumps(result) if format_json else result
 
 
 def filling_mode(array):
+    """Fills the missing values ​​by a mode"""
+
+
+    if type(array) is str:
+        array = json.loads(array)
     counters = collections.Counter(array)
 
     if math.nan in counters:
@@ -19,26 +37,36 @@ def filling_mode(array):
 
     mode = reduce(lambda a, x: x if counters[x] > counters[a] and not math.isnan(x) else a, counters)
 
-    return filling(array, mode)
+    return json.dumps(filling(array, mode))
 
 
 def filling_median(array):
+    """Fills the missing values ​​by a median"""
+
+    if type(array) is str:
+        array = json.loads(array)
     array_sorted = sorted(list(filter(lambda x: not math.isnan(x), array)))
     if len(array_sorted) % 2 == 0:
         median = (array_sorted[int(len(array_sorted) / 2)] + array_sorted[int(len(array_sorted) / 2 - 1)]) / 2
     else:
         median = array_sorted[int(len(array_sorted) / 2)]
 
-    return filling(array, median)
+    return json.dumps(filling(array, median))
 
 
 def filling(array, value):
-    return list(map(lambda x: x if not math.isnan(x) else value, array))
+    return list(map(lambda x: float(x) if not math.isnan(x) else float(value), array))
 
 
 def filling_grouping(array):
-    full = filling_aver(array)
+    """Filling in missed values ​​by average values ​​using data grouping"""
+
+    if type(array) is str:
+        array = json.loads(array)
+
+    full = filling_aver(array, False)
     size = int((len(array) + 1) / 2)
+
     while size >= 2:
         temp = []
 
@@ -47,7 +75,6 @@ def filling_grouping(array):
 
         full = temp
 
-        size = int( (size + 1) / 2)
+        size = int((size + 1) / 2)
 
-    return full
-
+    return json.dumps(full)
--- temperature_tools.py
+++ temperature_tools.py
@@ -1,23 +1,44 @@
+import json
 import math
 
 
-def average_day(min, max):
-    if math.isnan(min) or math.isnan(max):
+def average_day(minimum, maximum):
+    """Returns the average temperature per day"""
+
+    if type(minimum) is str:
+        minimum = float(minimum)
+
+    if type(maximum) is str:
+        maximum = float(maximum)
+
+    if math.isnan(minimum) or math.isnan(maximum):
         return -1, "insufficient data"
     else:
-        return 0, (min + max) / 2
+        return 0, (minimum + maximum) / 2
 
 
-def average_month(temp):
-    known_numbers = list(filter(lambda x: not math.isnan(x), temp))
+def average_month(array):
+    """Returns the average temperature for a month"""
 
-    if  len(known_numbers)<len(temp)/2:
+    if type(array) is str:
+        array = json.loads(array)
+
+    known_numbers = list(filter(lambda x: not math.isnan(x), array))
+
+    if len(known_numbers) < len(array) / 2:
         return -1, "insufficient data"
     else:
-        return 0 if len(known_numbers)<len(temp) else 1, sum(known_numbers)/len(known_numbers)
+        return 0 if len(known_numbers) < len(array) else 1, sum(known_numbers) / len(known_numbers)
 
 
-def average_daily_month(minimum, maxumum):
-    aver=[average_day(minimum[i], maxumum[i])[1] for i in range(max(len(minimum), len(maxumum)))]
+def average_daily_month(minimum, maximum):
+    """Returns the average temperature per month based on the minimum and maximum temperatures of each day"""
+
+    if type(minimum) is str:
+        minimum = json.loads(minimum)
+
+    if type(maximum) is str:
+        maximum = json.loads(maximum)
+
+    aver = [average_day(minimum[i], maximum[i])[1] for i in range(max(len(minimum), len(maximum)))]
     return average_month(aver)
-
